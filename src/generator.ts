35bf1ab35daf98427f4faef92eebafeb8f8c66d8
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-10-30 13:47:00 +0000

    feat: implement reciprocal rank fusion algorithm and add response generator prompt template

--- src/generator.ts
+++ src/generator.ts
@@ -1,14 +1,22 @@
 import { HumanMessage, SystemMessage, AIMessage } from "@langchain/core/messages";
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatMessagePromptTemplate, ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts"
+import { DocumentInterface } from "@langchain/core/documents";
 import dotenv from "dotenv";
+import { ChatGroq } from "@langchain/groq";
+import { Document } from "@langchain/core/documents";
 import z from "zod";
+import {formatDocumentsAsString} from 'langchain/util/document';
+import {response_generator_prompt} from "./prompts";
+
 import zodToJsonSchema from "zod-to-json-schema";
 import { queryVectorDB } from "./retriever";
+import { parse } from "path";
+import {reciprocalRankFusion} from "./RRF";
 dotenv.config();
 
 const llm = new ChatOpenAI({
-    model: 'gpt-5-mini',
+    model: 'gpt-4o-mini',
     configuration: {
         apiKey: process.env.OPENAI_API_KEY,
         baseURL: process.env.OPENAI_API_BASE_URL,
@@ -16,32 +24,67 @@ const llm = new ChatOpenAI({
 
     }
 })
+const groqLlm = new ChatGroq({
+    model: 'openai/gpt-oss-20b',
+    apiKey: process.env.GROQ_API_KEY,
+})
 
-const query = "What is few-shot?";
+const query = "What is prompt engineering?";
 
-const result = await queryVectorDB(query);
+// const result = await queryVectorDB(query);
 
-const prompt= PromptTemplate.fromTemplate(`
-You are an assistant for question-answering tasks. Use the following pieces of retrieved context
-to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum.
-Question: {question}
-Context: {context}
-Answer:
-`)
+const generate_question_prompt = PromptTemplate.fromTemplate(`
 
-const promptVal = await prompt.invoke({
-    question:query,
-    context:result[0]?.pageContent
+You are an AI search assistant.
+The user asked: {question}
+
+Step back and consider this question more broadly:
+1. Reframe it in general terms.
+2. Identify the main themes or dimensions involved.
+3. Generate 5 diverse search queries that cover these dimensions,
+ensuring each query explores a different perspective or phrasing.
+`);
+const generateQuestionPrompt = await generate_question_prompt.invoke({
+    question: query,
 
 })
 
-const llmResult = await llm.invoke([
+const structuredLlm = llm.withStructuredOutput(
+    z.object({
+        questions: z.array(z.string()).describe("Array of 5 diverse search queries"),
+    })
+);
+
+const llmResult = await structuredLlm.invoke([
     {
-        role:"user",
-        content: promptVal.value 
+        role: "user",
+        content: generateQuestionPrompt.value
+    }
+])
+const questions = llmResult.questions;
+
+const allRetrivedDocs = [] as Document[][];
+for (const question of questions) {
+    const result = await queryVectorDB(question);
+    allRetrivedDocs.push(result);
+}
+const fesedDoc = reciprocalRankFusion(allRetrivedDocs); // Take top 10 documents
+const docToString = formatDocumentsAsString(fesedDoc);
+
+const generatorResPrompt = await response_generator_prompt.invoke({
+    original_question: query,
+    questions: questions.join(", "),
+    retrieved_docs: docToString,
+})
+
+const aiResponse = await llm.invoke([
+    {
+        role: "user",
+        content: generatorResPrompt.value
     }
 ])
 
-console.log(llmResult)
+console.log("aiResponse:", aiResponse); // Add label to see what's returned
+// console.log(llmResult.questions); // This will give you the 5 questions array
 
 
--- src/retriever.ts
+++ src/retriever.ts
@@ -1,13 +1,14 @@
-import {Document} from "@langchain/core/documents"
-import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters"
-import {CohereEmbeddings} from "@langchain/cohere"
-import {PineconeStore} from "@langchain/pinecone"
-import {Pinecone as PineconeClientf} from "@pinecone-database/pinecone"
-import {CheerioWebBaseLoader} from "@langchain/community/document_loaders/web/cheerio"
+import { Document } from "@langchain/core/documents"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
+import { CohereEmbeddings } from "@langchain/cohere"
+import { PineconeStore } from "@langchain/pinecone"
+import { Pinecone as PineconeClientf } from "@pinecone-database/pinecone"
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio"
 import { CohereRerank } from "@langchain/cohere"
 import "dotenv/config"
+import {formatDocumentsAsString} from 'langchain/util/document';
 
-export async function queryVectorDB(query: string){
+export async function queryVectorDB(query: string) {
     const embeddings = new CohereEmbeddings({
         apiKey: process.env.COHERE_API_KEY,
         model: 'embed-english-v3.0',
@@ -24,15 +25,32 @@ export async function queryVectorDB(query: string){
         maxConcurrency: 5,
     })
 
-    const result = await vectorStore.similaritySearch(query, 10);
+    // const result = await vectorStore.similaritySearch(query, 10);
+    const retriever = await vectorStore.asRetriever();
+    const result = await retriever.invoke(query);
 
-    return result;
+    const cohereRerank = new CohereRerank({
+        apiKey: process.env.COHERE_API_KEY,
+        model: 'rerank-english-v3.0',
+    })
+
+    const rerankedDocuments = await cohereRerank.rerank(result, query, {
+        topN: 5
+    });
+
+    if (result.length > 0) {
+        return [result[rerankedDocuments[0].index]];
+    }else{
+        return [];
+    }
+    // console.log('Reranked Documents:', rerankedDocuments);
+    // return rerankedDocuments;
 
 
 }
 
-const result = await queryVectorDB("What is few-shot?");
-console.log(result);
+// const result = await queryVectorDB("What is few-shot?");
+// console.log(result);
 
 
 
