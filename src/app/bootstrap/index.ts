21f368f4143da85055b5cde92433b0762c8c0cc3
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-31 16:45:37 +0000

    feat: make bootStrapApp function asynchronous to include database connection

--- src/app/bootstrap/index.ts
+++ src/app/bootstrap/index.ts
@@ -2,11 +2,12 @@ import {Express} from 'express'
 import cors from 'cors'
 import express, { Request, Response } from 'express';
 import { expressServer } from './express/expressServer';
+import { dbConnection } from './mongos/dbConnection';
 
 
 
-export function bootStrapApp(app: Express, PORT:number) {
-
+export async function bootStrapApp(app: Express, PORT:number) {
+ await dbConnection()
  expressServer(app, PORT)
 
 }
