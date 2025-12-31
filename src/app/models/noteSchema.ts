be3484c3d9a7e0c8197f4d4304b0c947ccfd3911
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-31 16:51:38 +0000

    feat: add Mongoose schemas for documents, notes, and users

---
+++ src/app/models/noteSchema.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    image: { type: String, required: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+}, { timestamps: true });
+
+export const Note = mongoose.model("Note", noteSchema);
