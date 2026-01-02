c47950c721104ac1deb985dfaa40ee1085dd7f51
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-02 21:26:05 +0530

    feat: implement document loaders for PDF, HTML, and text files with chunking support

---
+++ src/app/http/controllers/notes/loaders/loaders.ts
@@ -0,0 +1,68 @@
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import path from "node:path";
+
+
+export async function splitDocToChunks(docs: Document<Record<string, any>>[], props: { chunkSize: number, chunkOverlap: number }) {
+    const splitter = new RecursiveCharacterTextSplitter({ ...props });
+    const splitDocs = await splitter.splitDocuments(docs);
+    return splitDocs
+
+}
+
+export async function loadWeb(url: string) {
+    const loader = new CheerioWebBaseLoader(url);
+    const docs = await loader.load();
+
+    return docs
+}
+
+export async function loadPDF(filePath: string) {
+    const loader = new PDFLoader(filePath);
+    const docs = await loader.load();
+
+    return docs
+}
+
+export async function loadText(filePath: string) {
+    const loader = new TextLoader(filePath);
+    const docs = await loader.load();
+    return docs
+}
+
+export async function loadDocument(
+    filePath: string,
+    doctype: "pdf" | "html" | "txt",
+    chunkSize = 800,
+    chunkOverlap = 200,
+) {
+    const extentionWithoutDot=path.extname(filePath).replace('.','') 
+    let docs = null;
+    switch (extentionWithoutDot.trim()) {
+        case 'pdf':
+            docs = await loadPDF(filePath);
+            break;
+        case 'html':
+            docs = await loadWeb(filePath);
+            break;
+        case 'txt':
+            docs = await loadText(filePath);
+            break;
+        default: throw new Error(`Unsupported file`);
+    }
+    return splitDocToChunks(docs, { chunkSize, chunkOverlap });
+}
+
+
+// export function getDocChunk (docSplit : any[]){ 
+//     const docChunk = [] as any 
+//     if (docSplit.length > 0) { 
+//         docChunk.push(docSplit[0]) 
+//     } else { 
+//         throw new Error('The provide Document is empty')  
+//     }
+//     return docChunk
+// } 
