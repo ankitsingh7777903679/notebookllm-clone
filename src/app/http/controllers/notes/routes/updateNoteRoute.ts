e78a05e6a9b37399363ef1b1e18fddf6ed44a273
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-02 21:26:50 +0530

    feat: implement updateNoteRoute for updating notes

---
+++ src/app/http/controllers/notes/routes/updateNoteRoute.ts
@@ -0,0 +1,12 @@
+import { Router } from "express" 
+import { updateNote } from "../updateNote"  
+
+
+
+
+export function updateNoteRoute(router: Router){  
+
+    
+    router.put('/notes', updateNote) 
+    return router
+}
