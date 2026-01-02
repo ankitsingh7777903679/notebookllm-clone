6120ecfc14d3a9050a699abb851c7d3bf6e07a56
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-02 21:27:08 +0530

    feat: fix import paths and enhance API v1 routes for notes

--- src/routes/apiV1.ts
+++ src/routes/apiV1.ts
@@ -1,13 +1,20 @@
+import { get } from 'lodash';
 import { driveRoutes } from "@/app/http/controllers/drive/routes/driveRoutes";
-import { createNoteRoute } from "@/app/http/controllers/notes/routes/createNoteRoutes";
+import { createNoteRoute } from "@/app/http/controllers/notes/routes/createNoteRoute";
+import { updateNoteRoute } from "@/app/http/controllers/notes/routes/updateNoteRoute";
 import { Router, Express } from "express";
+import { getAllNoteRoutes } from '@/app/http/controllers/notes/routes/getAllNoteRoute';
+import { summaryRoutes } from '@/app/http/controllers/notes/routes/summaryRoutes';
 
 
 
 
 export function apiV1(app:Express, router: Router){
     const driveRoute = driveRoutes(router)
-    const createNote=createNoteRoute(router)
+    const createNote= createNoteRoute(router)
+    const updateNote = updateNoteRoute(router)
+    const getAllNotesRoute = getAllNoteRoutes(router)
+    const summaryRoute = summaryRoutes(router)
 
-    app.use('/api/v1', driveRoute, createNote)
+    app.use('/api/v1', driveRoute, createNote, updateNote, getAllNotesRoute, summaryRoute)
 }
