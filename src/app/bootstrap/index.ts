b0e34eb2949eeeb80b60e6409a352bf5358e038c
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-02 21:24:17 +0530

    fix: import agenda and start it during app bootstrap

--- src/app/bootstrap/index.ts
+++ src/app/bootstrap/index.ts
@@ -3,11 +3,14 @@ import cors from 'cors'
 import express, { Request, Response } from 'express';
 import { expressServer } from './express/expressServer';
 import { dbConnection } from './mongos/dbConnection';
-
+import agenda from './agenda/agenda';
+import './agenda/jobs/imagejob'
 
 
 export async function bootStrapApp(app: Express, PORT:number) {
  await dbConnection()
+
+ await agenda.start();
  expressServer(app, PORT)
 
 }
