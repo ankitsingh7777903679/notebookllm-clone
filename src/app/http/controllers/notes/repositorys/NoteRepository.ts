c4bf12606b41149ae90e99ff88f3d8218790a8e3
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-02 21:26:19 +0530

    feat: enhance NoteRepository with image generation and pagination support for notes

--- src/app/http/controllers/notes/repositorys/NoteRepository.ts
+++ src/app/http/controllers/notes/repositorys/NoteRepository.ts
@@ -1,4 +1,6 @@
 // import { User } from "@/app/models/userSchema";
+
+import agenda from "@/app/bootstrap/agenda/agenda";
 import { generateTokens, signAccessToken, signRefreshToken } from "@/app/helpers/jwt";
 import { Note } from "@/app/models/noteSchema";
 import { User } from "@/app/models/userSchema";
@@ -18,16 +20,81 @@ export class NoteRepository {
 
     }
 
-    async createNote(props: { title: string, image: string, userId: string }) {
+    async createNote(noteProps: { title: string, image: string, userId: string },
+        imageProps: { generateImagePrompt: String, uploadsDir: String, randomName: String }
+    ) {
 
         const note = new Note({
-            ...props
+            ...noteProps
 
         })
         const newNote = await note.save();
+
+        agenda.now("generateImage", {
+            noteId: newNote.toObject()._id,
+            ...imageProps // required attr
+        });
         return newNote.toObject();
 
     }
+
+    async updateNotes(props: { id: string, title: string }) {
+        const updateNote = await Note.findByIdAndUpdate(props.id,
+            { title: props.title }, { new: true, runValidators: true });
+        return updateNote
+    }
+
+    async getA11Notes({
+        search = '',
+        page = 1,
+        limit = 10,
+    }: {
+
+        search?: string;
+        page?: number;
+        limit?: number;
+    }) 
+    {
+        // console.log("Search: ", search);
+        // console.log("Page: ", page);
+        // console.log("Limit: ", limit);
+        const skip = (page - 1) * limit; //.Build filter 
+        const filter: any = {};
+        if (search) {
+            filter.$or = [
+                {
+                    title: {
+                        $regex: search, $options: "i"
+                    }
+                },
+            ];
+        }
+
+        const [notes, total] = await Promise.all([
+            Note.find(filter)
+                .skip(skip)
+                .limit(limit)
+                .sort({ createdAt: -1 }) // newest first 
+                .lean(),
+            Note.countDocuments(filter),
+        ]);
+
+        return {
+            notes, pagination: {
+                total,
+                page,
+                limit,
+                totalPages: Math.ceil(total / limit),
+            },
+        };
+
+    }
+
+
+
+
+
+
 
 
 }
