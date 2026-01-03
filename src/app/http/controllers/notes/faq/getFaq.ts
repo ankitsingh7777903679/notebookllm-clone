f7d70d7d40dea1586c797167e735d317e3c43fd3
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-03 14:24:34 +0530

    feat: add getFaq function for retrieving FAQ from documents

---
+++ src/app/http/controllers/notes/faq/getFaq.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Express, NextFunction, Response, Request } from "express";
+import { DocRepository } from '../repositorys/DocRepository';
+
+
+
+
+
+
+export async function getFaq(req: Request, res: Response, next: NextFunction) {
+    try {
+
+        // steps :  
+        // -getFileName 
+        // -splitIntoChunks
+        // - call generateSummary 
+        // -storeSummaryInDB  
+        const {userId, noteId}:Record<string, any> = req.query;
+        const docRepo = DocRepository.getInstance()
+        const doc = await docRepo.getSingleDoc({ userId, noteId});
+        if(!doc) throw new Error("Document not found")
+
+
+        return res.status(200).send({faq:doc?.FAQ})
+
+
+    } catch (error) {
+        next(error)
+    }
+}
