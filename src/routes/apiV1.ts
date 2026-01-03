5444ea5e68fd88bdac2d3443b14cff7db78bccae
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-03 14:29:55 +0530

    refactor: reorganize imports and enhance apiV1 function for better route management

--- src/routes/apiV1.ts
+++ src/routes/apiV1.ts
@@ -5,16 +5,27 @@ import { updateNoteRoute } from "@/app/http/controllers/notes/routes/updateNoteR
 import { Router, Express } from "express";
 import { getAllNoteRoutes } from '@/app/http/controllers/notes/routes/getAllNoteRoute';
 import { summaryRoutes } from '@/app/http/controllers/notes/routes/summaryRoutes';
+import { briefingRoutes } from '@/app/http/controllers/notes/routes/briefingDocRoute';
+import { faqRoutes } from '@/app/http/controllers/notes/routes/faqRoute';
+import { studyGuideRoutes } from '@/app/http/controllers/notes/routes/studyGuideRoute';
+import { mindMapRoutes } from '@/app/http/controllers/notes/routes/mindMapRoute';
 
 
 
 
-export function apiV1(app:Express, router: Router){
+export function apiV1(app: Express, router: Router) {
     const driveRoute = driveRoutes(router)
-    const createNote= createNoteRoute(router)
+    const createNote = createNoteRoute(router)
     const updateNote = updateNoteRoute(router)
     const getAllNotesRoute = getAllNoteRoutes(router)
     const summaryRoute = summaryRoutes(router)
+    const briefingDocRoute = briefingRoutes(router)
+    const faqRoute = faqRoutes(router)
+    const studyGuideRoute = studyGuideRoutes(router)
+    const mindMapRoute = mindMapRoutes(router)
 
-    app.use('/api/v1', driveRoute, createNote, updateNote, getAllNotesRoute, summaryRoute)
+
+    app.use('/api/v1', driveRoute, createNote, updateNote, getAllNotesRoute, summaryRoute, briefingDocRoute,
+        briefingDocRoute, faqRoute, studyGuideRoute, mindMapRoute, mindMapRoute
+    )
 }
