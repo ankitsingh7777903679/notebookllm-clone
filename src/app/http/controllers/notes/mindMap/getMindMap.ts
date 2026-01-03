5a18a92a14c879ce403dbcbde17a2ce20b6685ff
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-03 14:26:55 +0530

    feat: implement getMindMap function for retrieving mind maps from documents

---
+++ src/app/http/controllers/notes/mindMap/getMindMap.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Express, NextFunction, Response, Request } from "express";
+import { DocRepository } from '../repositorys/DocRepository';
+
+
+
+
+
+
+export async function getMindMap(req: Request, res: Response, next: NextFunction) {
+    try {
+
+        // steps :  
+        // -getFileName 
+        // -splitIntoChunks
+        // - call generateSummary 
+        // -storeSummaryInDB  
+        const {userId, noteId}:Record<string, any> = req.query;
+        const docRepo = DocRepository.getInstance()
+        const doc = await docRepo.getSingleDoc({ userId, noteId});
+        if(!doc) throw new Error("Document not found")
+
+
+        return res.status(200).send({mindMap:doc?.mindMap})
+
+
+    } catch (error) {
+        next(error)
+    }
+}
